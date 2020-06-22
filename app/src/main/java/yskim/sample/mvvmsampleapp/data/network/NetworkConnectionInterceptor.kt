ab92e9e4f6f61532483a5efeb0fe36db7e9d2f38
김윤수 <yskim@taeha.co.kr>
2020-06-22 11:32:19 +0900

    네트워크 예외처리2

--- app/src/main/java/yskim/sample/mvvmsampleapp/data/network/NetworkConnectionInterceptor.kt
+++ app/src/main/java/yskim/sample/mvvmsampleapp/data/network/NetworkConnectionInterceptor.kt
@@ -2,9 +2,12 @@ package yskim.sample.mvvmsampleapp.data.network
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import okhttp3.Interceptor
 import okhttp3.Response
 import yskim.sample.mvvmsampleapp.util.NoInternetException
+
 
 class NetworkConnectionInterceptor(
     context: Context
@@ -19,14 +22,42 @@ class NetworkConnectionInterceptor(
         return chain.proceed(chain.request())
     }
 
-    private fun isInternetAvailable() : Boolean {
+//    private fun isInternetAvailable() : Boolean {
+//        val connectivityManager =
+//            applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+//        connectivityManager.activeNetworkInfo.also {
+//            return it != null && it.isConnected
+//        }
+//    }
 
+    private fun isInternetAvailable(): Boolean {
+        var result = false
         val connectivityManager =
             applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val networkCapabilities = connectivityManager.activeNetwork ?: return false
+            val actNw =
+                connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+            result = when {
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        } else {
+            connectivityManager.run {
+                connectivityManager.activeNetworkInfo?.run {
+                    result = when (type) {
+                        ConnectivityManager.TYPE_WIFI -> true
+                        ConnectivityManager.TYPE_MOBILE -> true
+                        ConnectivityManager.TYPE_ETHERNET -> true
+                        else -> false
+                    }
 
-        connectivityManager.activeNetworkInfo.also {
-            return it != null && it.isConnected
+                }
+            }
         }
 
+        return result
     }
 }
